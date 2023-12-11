9ea4f4f767a5748c3fd82cef2e4a697b675825b7
Daniel Martins <danielm94@users.noreply.github.com>
2023-12-10 19:05:57 -0500

    Add code for spring context demo

--- src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
+++ src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6di;
 
+import guru.springframework.spring6di.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring6DiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring6DiApplication.class, args);
+        var ctx = SpringApplication.run(Spring6DiApplication.class, args);
+
+        MyController controller = ctx.getBean(MyController.class);
+        System.out.println("In main method");
+        System.out.println(controller.sayHello());
     }
 
 }
