d2f1efb611dddaaaafba38858c3012290ae8beb3
Daniel Martins <danielm94@users.noreply.github.com>
2023-12-11 16:09:49 -0500

    Add GreetingService to MyController

--- src/main/java/guru/springframework/spring6di/controllers/MyController.java
+++ src/main/java/guru/springframework/spring6di/controllers/MyController.java
@@ -1,11 +1,18 @@
 package guru.springframework.spring6di.controllers;
 
+import guru.springframework.spring6di.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String sayHello() {
         System.out.println("I'm in the controller.");
-        return "Hello everyone!";
+        return greetingService.sayGreeting();
     }
 }
